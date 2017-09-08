51dcfe2acde54dea1065823b70d0cafd6ed82a46
Mani Jaganathan <Manimegalai.Jaganathan@bskyb.com>
2017-09-08 09:46:40 +0100

    solutions for 3 and 4

--- problems1to5.py
+++ problems1to5.py
@@ -1,6 +1,5 @@
 __author__ = 'mani'
 
-'''
 #problem1:If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 #solution1:straight forward method
@@ -25,7 +24,7 @@ def natural1B():
 # find the sum of the even-valued terms.
 
 #solution1:find the fibonacci within 4 million(4000000)
-deg fibonacci2():
+def fibonacci2():
     def fib(limit):
         sum = 0
         a = 1
@@ -41,14 +40,12 @@ deg fibonacci2():
 
     solution = reduce(lambda a,b: a+b,[val for val in fib(limit = 4000000)])
     print "solution for fibonacii series upto 4 million is {}".format(solution)
-'''
-'''
 
 #problem3:The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
 #solution1:
-deg primefactor3():
+def primefactor3():
     import math
     n = 600851475143
     #n = 13195
@@ -73,8 +70,8 @@ deg primefactor3():
         factor = factor + 2
 
     print "lastfactor is {}".format(lastfactor)
-'''
-'''
+
+
 #problem4:A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009
 #91times99.
 #Find the largest palindrome made from the product of two 3-digit numbers.
@@ -93,8 +90,8 @@ def palindrome4():
     return largestPalindrome
 
 palindrome4()
-'''
-'''
+
+
 #problem5:2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 #What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
@@ -109,7 +106,14 @@ def small5():
 
 if __name__ == '__main__':
     import timeit
+
+    natural1A()
+    natural1B()
+    fibonacci2()
+    primefactor3()
+    palindrome4()
     print(timeit.timeit(small5,number =1))
-'''
+
+
 
 
